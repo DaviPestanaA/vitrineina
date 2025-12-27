c89584befaf59e554ab06f5e4238714ecb0828f4
DaviPestanaA <oficialpestana@gmail.com>
2025-12-27 20:30:51 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <div style={{ padding: 40, fontSize: 28 }}>
+    FUNCIONOU ✅
+  </div>
 );
